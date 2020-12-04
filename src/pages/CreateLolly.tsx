05b0f2497200f87ded043af0610d2f60204faae5
unknown <hammad_205@hotmail.com>
2020-12-05 01:55:39 +0500

    add button and styles

---
+++ src/pages/CreateLolly.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateLolly = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default CreateLolly
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import React from "react"
 //@ts-ignore
 import img from "./bg.png"
-import './../global.css'
+import './../styles/global.css'
 import Image from 'react-bootstrap/Image'
 import Header from "../components/Header"
 import { Button } from "react-bootstrap"
+import { Link } from 'gatsby';
 
 export default function Home() {
-  return <div className="a">
+  return <div>
     <div className='home-container'>
       <Header />
     <Image className='home-lolly' src={img} fluid />
-    <Button className='home-btn' size="lg" disabled>
+    <button onClick={() => <Link to='/CreateLolly' />} className='home-btn' size="lg">
     Make a new lolly to send to a friend
-  </Button>
+  </button>
     </div>
     
   </div>
